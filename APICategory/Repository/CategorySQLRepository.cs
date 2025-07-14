ea6e428eab74b3f15e959e7c2f99f6779f840164
imararecase <danign20@hotmail.com>
2025-07-13 22:55:12 -0500

    Add: Category repository interface and CRUD implementation
    Se creó la interfaz y la implementación del repositorio para manejar las operaciones CRUD de categoría : listar, obtener por ID, crear, actualizar y eliminar.

---
+++ APICategory/Repository/CategorySQLRepository.cs
@@ -0,0 +1,62 @@
+﻿using APICategory.DbContexts;
+using APICategory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICategory.Repository
+{
+    public class CategorySQLRepository : ICategoryRepository
+    {
+        private AppDbContext dbContext;
+        public CategorySQLRepository(AppDbContext dbContext) 
+        {
+            this.dbContext = dbContext;
+        }
+
+        // Listado de categorias
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            var result = await this.dbContext.Categories.ToListAsync();
+            if (result == null)
+            {
+                throw new Exception("No categories found");
+            }
+            return result;
+        }
+
+        // Obtener una categoria por Id
+        public async Task<Category> GetCategoryById(int categoryId)
+        {
+            var category = await dbContext.Categories.FirstOrDefaultAsync(c => c.CategoryId == categoryId);
+            return category;
+        }
+
+        // Crear una categoria
+        public async Task<Category> CreateCategory(Category category)
+        {
+            dbContext.Categories.Add(category);
+            await dbContext.SaveChangesAsync();
+            return category;
+        }
+
+        // Actualizar una categoria
+        public async Task<Category> UpdateCategory(Category category)
+        {
+            dbContext.Categories.Update(category);
+            await dbContext.SaveChangesAsync();
+            return category;
+        }
+
+        // Eliminar una categoria
+        public async Task<bool> DeleteCategory(int categoryId)
+        {
+            var category = await dbContext.Categories.FirstOrDefaultAsync(c => c.CategoryId == categoryId);
+            if(category == null)
+            {
+                return false;
+            }
+            dbContext.Categories.Remove(category);
+            await dbContext.SaveChangesAsync();
+            return true;
+        }        
+    }
+}
---
+++ APICategory/Repository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using APICategory.Models;
+
+namespace APICategory.Repository
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetCategoryById(int categoryId);
+        Task<Category> CreateCategory(Category category);
+        Task<Category> UpdateCategory(Category category);
+        Task<bool> DeleteCategory(int categoryId);
+    }
+}
